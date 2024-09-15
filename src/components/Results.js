7c2b684e55fd52305e6bbac75cb2b54525cc89ee
Aniket Hirpara <gdg4dev@gmail.com>
2024-09-15 07:58:19 -0400

    added a demo for Sexual offender

--- src/components/Results.js
+++ src/components/Results.js
@@ -15,47 +15,51 @@ const Results = ({ results, status }) => {
       )}
       {status === "fetched" && results.length > 0 && (
         <div className="results-grid">
-          {results.map((result, index) => (
-            <div key={index} className="result-card">
-              <h3>{result.name}</h3>
-              <div className="result-field">
-                {result.phone && (
-                  <p>
-                    <strong>Phone:</strong> {result.phone}
-                  </p>
-                )}
+          {results.map((result, index) => {
+         
+            return (
+              <div key={index} className="result-card">
+                <h3>{result.name}</h3>
+               {result.offenderLink && (<h6 style={{color: 'white'}}>Similar profile has been found on <a style={{color: 'white'}} href="https://vspsor.com/Offender/Details/94983283-7402-4e07-9999-53084e5ff094">Sex offender lookup</a></h6>)} 
+                <div className="result-field">
+                  {result.phone && (
+                    <p>
+                      <strong>Phone:</strong> {result.phone}
+                    </p>
+                  )}
+                </div>
+                <div className="result-field">
+                  {result.email && (
+                    <p>
+                      <strong>Email:</strong> {result.email}
+                    </p>
+                  )}
+                </div>
+                <div className="result-field">
+                  {result.street && result.city && result.state && result.zip && (
+                    <p>
+                      <strong>Address:</strong> {result.street}, {result.city},{" "}
+                      {result.state} {result.zip}
+                    </p>
+                  )}
+                </div>
+                <div className="result-field">
+                  {result.age && (
+                    <p>
+                      <strong>Age:</strong> {result.age}
+                    </p>
+                  )}
+                </div>
+                <div className="result-field">
+                  {result.birthday && (
+                    <p>
+                      <strong>Birthday:</strong> {result.birthday}
+                    </p>
+                  )}
+                </div>
               </div>
-              <div className="result-field">
-                {result.email && (
-                  <p>
-                    <strong>Email:</strong> {result.email}
-                  </p>
-                )}
-              </div>
-              <div className="result-field">
-                {result.street && result.city && result.state && result.zip && (
-                  <p>
-                    <strong>Address:</strong> {result.street}, {result.city},{" "}
-                    {result.state} {result.zip}
-                  </p>
-                )}
-              </div>
-              <div className="result-field">
-                {result.age && (
-                  <p>
-                    <strong>Age:</strong> {result.age}
-                  </p>
-                )}
-              </div>
-              <div className="result-field">
-                {result.birthday && (
-                  <p>
-                    <strong>Birthday:</strong> {result.birthday}
-                  </p>
-                )}
-              </div>
-            </div>
-          ))}
+            )
+          })}
         </div>
       )}
     </div>
